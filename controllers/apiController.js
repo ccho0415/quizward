c504c0d92dbf7748aa0a932e01c5d78bf80e55df
thechutrain <chu.chuyalan@gmail.com>
2017-03-13 15:24:33 -0400

    added category query

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,15 +1,50 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+// Get access to db
+var Models = require('../models');
+
 // Create Router Object & middleware
 var router = express.Router();
 var jsonParse = bodyParser.urlencoded({ extended: false });
 router.use(jsonParse);
 
 
-// Routers
+// API HOME  ----------------------------------- //
+router.get('/', function(req, res) {
+  res.render('api.handlebars');
+})
+
+// CATEGORY  ----------------------------------- //
+/**
+ * "/categories/:id?*" {GET} - returns all categories or given an id, returns that category specifically
+ */
+router.get('/categories/:id?', (req, res) => {
+  // 1.) if there's a category id in query string get it
+  category_id = parseInt(req.params.id);
+
+  // 2) Query database
+  if (category_id) {
+    Models.Category.findOne(
+      { where: { id: category_id} }
+    ).then((result) => {
+      res.json(result);
+    })
+  } else {
+    Models.Category.findAll({}).then((results) => {
+      res.json(results);
+    });
+  }
+});
+
+// USER ----------------------------------- //
 router.get('/', function(req, res) {
   res.json({ errors: false });
 })
+
+
+// QUIZ ----------------------------------- //
+
+
 
 module.exports = router;
