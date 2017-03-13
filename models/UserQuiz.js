589fca032e06a3564e949dee56bed318521faf1d
thechutrain <chu.chuyalan@gmail.com>
2017-03-13 18:18:38 -0400

    original userquiz table

--- models/UserQuiz.js
+++ models/UserQuiz.js
@@ -10,34 +10,6 @@ module.exports = function(sequelize, DataTypes) {
       {
         underscored: true,
         freezeTableName: true,
-        // classMethods: {
-        //   associate: function(models) {
-        //     UserQuiz.hasMany(models.Comment);
-        //   }
-        // }
       }) // end of define
   return UserQuiz;
 };
-
-// module.exports = function(sequelize, DataTypes) {
-//   var UserQuiz = sequelize.define('UserQuiz',
-//       // column names
-//       {
-//         score: {
-//           type: DataTypes.DECIMAL(5, 2),
-//         }
-//       },
-//       // options
-//       {
-//         underscored: true,
-//         freezeTableName: true,
-//         classMethods: {
-//           associate: function(models) {
-//             UserQuiz.hasMany(models.Post, {
-//               onDelete: 'CASCADE'
-//             });
-//           }
-//         } // end classMethods
-//       }) // end of define
-//   return UserQuiz;
-// };
